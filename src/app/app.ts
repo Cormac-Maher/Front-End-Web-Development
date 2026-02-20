399d465997a42bb83522bae55e3fec5e16f6b0c3
unknown <G00463317@atu.ie>
2026-02-20 13:19:33 +0000

    Lab 3

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {ComponentA} from './component-a/component-a'      // importing components
+import {ComponentB} from './component-b/component-b'
+// importing components A and B
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'root-directive',
+  imports: [RouterOutlet, ComponentA, ComponentB],    // setting imports in component class
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('my-first-app');
+export class App {            // setting app class with name and age variables
+  name: string = "Cormac";
+  age: number = 20;
 }
