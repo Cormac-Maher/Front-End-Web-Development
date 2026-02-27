53e2a34cd759f0d0a4d0f5adcfd608e7ff8f2ee2
unknown <G00463317@atu.ie>
2026-02-27 13:34:56 +0000

    lab4 complete

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Listcomponent } from './listcomponent/listcomponent';    // imported list component
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Listcomponent],      // imported list component
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('data-app');
+  count:number = 0;
+  hide:boolean = true;
+
+  onButtonClick(){                    // method for mutton clicked
+    alert("Button was clicked");
+    this.count++;
+  }
+
+  onDoubleClick(){                 // method for unhiding text on double click
+    if(this.hide === true){
+      this.hide = false;
+    } else{
+      this.hide = true;
+    } 
+  }  
 }
